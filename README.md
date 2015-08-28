c2b9827817e238ed8bded8c3e80970a20ef1f14a
pudymody <federicoscodelaro@gmail.com>
2015-08-28 13:55:22 -0300

    Base code

---
+++ README.md
@@ -0,0 +1,2 @@
+# tinderSwipe
+Tinder swipe effect with css3. For more info visit the [blog post](http://pudymody.github.io/2015/08/09/tinder-like-swipe.html)
---
+++ script.js
@@ -0,0 +1,38 @@
+var $VIEW = document.querySelector('.view');
+function whichTransitionEvent(){
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+        'animation':'animationend',
+        'OAnimation':'oAnimationEnd',
+        'MozAnimation':'mozAnimationEnd',
+        'WebkitAnimation':'webkitAnimationEnd'
+    };
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
+}
+/* Listen for a transition! */
+var transitionEvent = whichTransitionEvent();
+$VIEW.addEventListener(transitionEvent, function(e){
+    e.target.remove();
+    getPerson();
+});
+function add( name ){
+    $VIEW.children[0].className += ' '+name;
+}
+function getPerson(cb){
+    var url = "https://randomuser.me/api/?gender=female";
+    var oReq = new XMLHttpRequest();
+        oReq.addEventListener('load', function(){
+            var data = JSON.parse(this.responseText);
+            data = data.results[0].user;
+            var html = '<div class="user-card"><img class="user-card__picture" src="'+data.picture.large+'"></img><h1 class="user-card__name">'+data.name.first+' '+data.name.last+'</h1></div>';
+            $VIEW.innerHTML = html;
+        });
+        oReq.open("get", url, true);
+        oReq.send();
+}
+getPerson();
